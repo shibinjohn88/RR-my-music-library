fa81e4372c19ffdb5a04613eaada23022c26d0d8
shibinjohn88 <shibinjohnpdlm@gmail.com>
2023-01-06 06:10:43 -0500

    data moved to context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
-
+import { DataContext } from './context/DataContext'
 
 const App = () => {
     let [search, setSearch] = useState('')
@@ -35,7 +35,9 @@ const App = () => {
         <div>
             <SearchBar handleSearch={handleSearch} />
             {message}
-            <Gallery data={data} />
+            <DataContext.Provider value={data}>
+                <Gallery />
+            </DataContext.Provider>
         </div>
     )
 }
